5e446b5ab3144884784e93a986acc01bf747a0bc
weston-bailey <wdbailey87@gmail.com>
2022-09-27 12:30:32 -0700

    lunch

---
+++ src/Joke.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Joke extends Component {
+
+    componentDidMount() {}
+
+    componentDidUpdate() {}
+
+    componentWillUnmount() {}
+    
+    render() {
+        console.log('Joke.js is rendering!')
+        return (
+            <div>
+                Joke Component
+            </div>
+        )
+    }
+}
